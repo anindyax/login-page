3346215c05c718f872a7180fba6f7a76cb580719
Anindya Dutta <anindyadutta1812@gmail.com>
2022-05-31 12:51:17 +0530

    Second commit

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ if (isset($_POST['submit'])) {
 
 	<link rel="stylesheet" type="text/css" href="style.css">
 
-	<title>Login Form </title>
+	<title>Login Form - Pure Coding</title>
 </head>
 <body>
 	<div class="container">
